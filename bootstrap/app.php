ce6824ca4421b5b0bcbf66487ec745c859128025
Yanselmask <jansel48@hotmail.com>
2024-07-27 21:39:01 +0200

    fixed composer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web([]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
