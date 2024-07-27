1a414148b75dad8b4f0f03de4d3cdc9d5a88dbb6
Yanselmask <jansel48@hotmail.com>
2024-07-27 20:56:12 +0200

    ready for submitted

--- resources/views/vendor/spark/app.blade.php
+++ resources/views/vendor/spark/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ site_name() }}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css?family=Nunito:400,600,700" rel="stylesheet">
