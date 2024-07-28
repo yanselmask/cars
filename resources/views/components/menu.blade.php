cb0cbe24d866a4cc3eba482ef1da780bd27bb513
Yanselmask <jansel48@hotmail.com>
2024-07-28 07:27:25 +0200

    fixed menu actived

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -5,7 +5,7 @@
         <!-- Menu items-->
         @foreach(menu($menu)->items as $item)
         <li class="nav-item @if($item['children']) dropdown @endif">
-            <a class="nav-link @if($item['children']) dropdown-toggle @endif @isset($item['data']['classes']) {{$item['data']['classes']}} @endisset" href="{{$item['data']['url']}}" target="{{$item['data']['target']}}">
+            <a class="nav-link @if(strlen($item['data']['url']) == 1 && request()->routeIs('home')) active @endif @if(request()->path() == substr($item['data']['url'], 1)) active @endif @if($item['children']) dropdown-toggle @endif @isset($item['data']['classes']) {{$item['data']['classes']}} @endisset" href="{{$item['data']['url']}}" target="{{$item['data']['target']}}">
                @if(isset($item['data']['icon']) && (isset($item['data']['icon_position']) && $item['data']['icon_position'] == 'left') ) <i class="{{$item['data']['icon']}} me-2"></i>@endif {{ $item['label'] }} @if(isset($item['data']['icon']) && (isset($item['data']['icon_position']) && $item['data']['icon_position'] == 'right') ) <i class="{{$item['data']['icon']}} me-2"></i> @endif
                    @if(isset($item['data']['divider']) && $item['data']['divider'])
                        <span class="d-none d-lg-block position-absolute top-50 end-0 translate-middle-y border-end border-light" style="width: 1px; height: 30px;"></span>
